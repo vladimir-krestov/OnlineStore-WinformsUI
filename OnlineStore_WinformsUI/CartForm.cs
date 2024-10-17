e5bf8d9881ccf4323481befd702ce355426f308c
Vladimir Krestov <vladimir.krestov@akvelon.com>
2024-10-17 15:01:54 +0200

    Added Cart

---
+++ OnlineStore_WinformsUI/CartForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlineStore_WinformsUI
+{
+    public partial class CartForm : Form
+    {
+        public CartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            cartPizzaTitleLabel = new Label();
+            cartPizzaDescriptionLabel = new Label();
+            pictureBox1 = new PictureBox();
+            numericUpDown1 = new NumericUpDown();
+            cartPizzaTotalLabel = new Label();
+            cartCompleteButton = new Button();
+            panel1.SuspendLayout();
+            ((ISupportInitialize)pictureBox1).BeginInit();
+            ((ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.LavenderBlush;
+            panel1.Controls.Add(cartPizzaTotalLabel);
+            panel1.Controls.Add(numericUpDown1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(cartPizzaDescriptionLabel);
+            panel1.Controls.Add(cartPizzaTitleLabel);
+            panel1.Location = new Point(33, 35);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(350, 151);
+            panel1.TabIndex = 0;
+            // 
+            // cartPizzaTitleLabel
+            // 
+            cartPizzaTitleLabel.AutoSize = true;
+            cartPizzaTitleLabel.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cartPizzaTitleLabel.Location = new Point(150, 25);
+            cartPizzaTitleLabel.Name = "cartPizzaTitleLabel";
+            cartPizzaTitleLabel.Size = new Size(62, 31);
+            cartPizzaTitleLabel.TabIndex = 1;
+            cartPizzaTitleLabel.Text = "Title";
+            // 
+            // cartPizzaDescriptionLabel
+            // 
+            cartPizzaDescriptionLabel.AutoSize = true;
+            cartPizzaDescriptionLabel.Location = new Point(150, 70);
+            cartPizzaDescriptionLabel.Name = "cartPizzaDescriptionLabel";
+            cartPizzaDescriptionLabel.Size = new Size(85, 20);
+            cartPizzaDescriptionLabel.TabIndex = 1;
+            cartPizzaDescriptionLabel.Text = "Description";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(22, 25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 100);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(258, 109);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(52, 27);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // cartPizzaTotalLabel
+            // 
+            cartPizzaTotalLabel.AutoSize = true;
+            cartPizzaTotalLabel.Location = new Point(150, 111);
+            cartPizzaTotalLabel.Name = "cartPizzaTotalLabel";
+            cartPizzaTotalLabel.Size = new Size(45, 20);
+            cartPizzaTotalLabel.TabIndex = 3;
+            cartPizzaTotalLabel.Text = "0 Rsd";
+            // 
+            // cartCompleteButton
+            // 
+            cartCompleteButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            cartCompleteButton.BackColor = Color.FromArgb(255, 224, 192);
+            cartCompleteButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cartCompleteButton.Location = new Point(420, 446);
+            cartCompleteButton.Name = "cartCompleteButton";
+            cartCompleteButton.Size = new Size(121, 50);
+            cartCompleteButton.TabIndex = 1;
+            cartCompleteButton.Text = "Complete";
+            cartCompleteButton.UseVisualStyleBackColor = false;
+            // 
+            // CartForm
+            // 
+            ClientSize = new Size(567, 520);
+            Controls.Add(cartCompleteButton);
+            Controls.Add(panel1);
+            Name = "CartForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((ISupportInitialize)pictureBox1).EndInit();
+            ((ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        private Panel panel1;
+        private Label cartPizzaTotalLabel;
+        private NumericUpDown numericUpDown1;
+        private PictureBox pictureBox1;
+        private Label cartPizzaDescriptionLabel;
+        private Button cartCompleteButton;
+        private Label cartPizzaTitleLabel;
+    }
+}
--- OnlineStore_WinformsUI/Form1.cs
+++ OnlineStore_WinformsUI/Form1.cs
@@ -1,14 +1,13 @@
 using Newtonsoft.Json;
 using OnlineStore_WinformsUI.Models;
 using OnlineStore_WinformsUI.Services;
-using System.Net.Http;
-using System.Text.Json;
 
 namespace OnlineStore_WinformsUI
 {
     public partial class Form1 : Form
     {
         private const int OrdersPageSize = 20;
+        private double CurrentOrderTotal = 0;
 
         private static readonly ApiClient apiClient = new ApiClient(); // Disposed in Dispose method in the Designer file.
 
@@ -144,7 +143,7 @@ namespace OnlineStore_WinformsUI
 
         private async void generateOrdersButton_Click(object sender, EventArgs e)
         {
-            await new OrderGenerator(apiClient, ordersGenerationProgressBar).GenerateNewOrderItems(50000);
+            await new OrderGenerator(apiClient, ordersGenerationProgressBar).GenerateNewOrderItems(25000);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -152,6 +151,112 @@ namespace OnlineStore_WinformsUI
             // Do not use authentication
             loginPanel.Visible = false;
             mainPanel.Visible = true;
+        }
+
+        private void cartButton_Click(object sender, EventArgs e)
+        {
+            CartForm cartForm = new CartForm();
+            cartForm.ShowDialog();
+        }
+
+        private async void adminPageTabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (sender != adminPageTabControl)
+            {
+                return;
+            }
+
+            if (adminPageTabControl.SelectedTab != pizzasTabPage)
+            {
+                return;
+            }
+
+            string? response = await apiClient.GetDataAsync("Pizza/GetAllPizza");
+
+            if (response is null)
+            {
+                pizzasErrorLabel.Text = "Error getting pizzas";
+                return;
+            }
+
+            List<Pizza>? items = JsonConvert.DeserializeObject<List<Pizza>>(response);
+            if (items is null)
+            {
+                pizzasErrorLabel.Text = "Error serializing a list of pizzas";
+                return;
+            }
+
+            pizzasFlowLayoutPanel.Controls.Clear();
+
+            foreach (var pizza in items)
+            {
+                PictureBox pictureBox = new();
+                Panel panelItem = new();
+                NumericUpDown numericUpDown = new();
+                Label priceLabel = new();
+                Label titleLabel = new();
+
+                // 
+                // pizzaPanelPictureBox
+                // 
+                pictureBox.Location = new Point(49, 9);
+                pictureBox.Name = "pizzaPanelPictureBox";
+                pictureBox.Size = new Size(180, 180);
+                pictureBox.TabStop = false;
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                //
+                // pizzaPanelItem
+                //
+                panelItem.Controls.Add(pictureBox);
+                panelItem.Controls.Add(numericUpDown);
+                panelItem.Controls.Add(priceLabel);
+                panelItem.Controls.Add(titleLabel);
+                panelItem.Location = new Point(3, 3);
+                panelItem.Name = "pizzaPanelItem";
+                panelItem.Size = new Size(270, 270);
+                // 
+                // pizzaPanelNumericUpDown
+                // 
+                numericUpDown.Location = new Point(172, 229);
+                numericUpDown.Name = "pizzaPanelNumericUpDown";
+                numericUpDown.Size = new Size(67, 27);
+                numericUpDown.ValueChanged += (_, args) =>
+                {
+                    cartButton.Text = $"My order | {CurrentOrderTotal += pizza.Price}";
+                };
+                // 
+                // pizzaPanelPriceLabel
+                // 
+                priceLabel.AutoSize = true;
+                priceLabel.Location = new Point(49, 231);
+                priceLabel.Name = "pizzaPanelPriceLabel";
+                priceLabel.Size = new Size(45, 20);
+                priceLabel.Text = $"{pizza.Price} Rsd";
+                // 
+                // pizzaPanelTitleLabel
+                // 
+                titleLabel.AutoSize = true;
+                titleLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+                titleLabel.Name = "pizzaPanelTitleLabel";
+                titleLabel.Size = new Size(100, 20);
+                titleLabel.AutoSize = true;
+                titleLabel.TextAlign = ContentAlignment.MiddleCenter;
+                titleLabel.Text = pizza.Title;
+                titleLabel.Location = new Point(
+                    (panelItem.Width - titleLabel.Width) / 2,
+                    197
+                );
+
+                pizzasFlowLayoutPanel.Controls.Add(panelItem);
+
+                Task.Run(async () => pictureBox.Image = await apiClient.GetImageData(pizza.ImageUrl));
+            }
+        }
+
+
+        private void pizzaPanelNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            return; //
         }
     }
 }
